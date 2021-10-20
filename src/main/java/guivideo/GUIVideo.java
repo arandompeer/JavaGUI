6591623fb9461c96b5432a2b61be4d1e6b40d065
arandompeer <46127698+arandompeer@users.noreply.github.com>
2021-10-20 20:07:42 +0200

    Add files via upload
    Adding files

---
+++ src/main/java/guivideo/GUIVideo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guivideo;
+
+/**
+ *
+ * @author 27722
+ */
+public class GUIVideo {
+    public static void main(String[] args) {
+        new Menu().setVisible(true);
+    }
+}
